c00938f327c3d16f4e7a4207dbd36b717a41acd2
Rodrigo-Alonzo <ralonzo1337@gmail.com>
2023-05-23 17:34:37 -0300

    Nueva actualizacion de web

---
+++ main.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 450) {
+      document.getElementById("navbar").classList.add("nav-fixed");
+      document.getElementById("navbar").classList.remove("navbar");
+      navbar_height = document.querySelector(".navbar");
+      document.body.style.paddingTop = navbar_height + "px";
+    } else {
+      document.getElementById("navbar").classList.remove("nav-fixed");
+      document.getElementById("navbar").classList.add("navbar");
+      document.body.style.paddingTop = "0";
+    }
+  });
+});
+
