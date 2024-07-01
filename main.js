a7f4dd97f37a86378f9cccc902f6d0569749ac53
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-07-01 04:56:40 -0300

    reorganizacion de logica

--- main.js
+++ main.js
@@ -1,40 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
+  var icon = document.getElementById("bars");
+  const skillSpan = document.getElementById("span");
+  const skillBtn = document.getElementById("skillButton");
+  function changeIcon(element,c1,c2) {
+    element.classList.toggle(c1)
+    element.classList.toggle(c2)
+  };
+  skillBtn.addEventListener("click", spanShow,);
+  document.getElementById("btn-nav").addEventListener("click", () => changeIcon(icon,'la-bars','la-times'));
   window.addEventListener("scroll", function () {
-    if (window.scrollY > 430) {
-      document.getElementById("navbar").classList.add("nav-fixed");
-      document.getElementById("navbar").classList.remove("navbar");
-      navbar_height = document.querySelector(".navbar");
-      document.body.style.paddingTop = navbar_height + "px";
-    } else {
-      document.getElementById("navbar").classList.remove("nav-fixed");
-      document.getElementById("navbar").classList.add("navbar");
-      document.body.style.paddingTop = "0";
-    }
+    if (window.scrollY > 430) document.querySelector(".navbar-brand").innerHTML="RA"
+    else document.querySelector(".navbar-brand").innerHTML="Rodrigo Alonzo"
   });
 });
-document.getElementById("btn-nav").addEventListener("click", changeIcon);
-
-var icon = document.getElementById("bars");
-
-function changeIcon() {
-icon.classList.toggle("la-bars")
-icon.classList.toggle("la-times")  
-};
-
-const skillSpan = document.getElementById("span");
-const skillBtn = document.getElementById("skillButton");
-
-skillBtn.addEventListener("click", spanShow,);
-
-function spanShow() {
-
-  skillSpan.classList.toggle("tailwindSpan");
-  skillSpan.classList.toggle("tailwindSpan:active");
-}
-
-
-function mostrar () { 
-const span = document.getElementById("viteSpan"); 
-span.classList.toggle("viteSpan");
-span.classList.toggle("viteSpan:active");
-}
