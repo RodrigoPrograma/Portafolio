8fd5e87ed6ed29b33e7b9cf8f199feb11f5a7b81
Rodrigo <ralonzo1337@gmail.com>
2024-07-23 14:53:16 -0300

    Added PHP funtion ges. Lines starting

---
+++ php/Form.php
@@ -0,0 +1,22 @@
+<?php
+$to = "ralonzo1337@gmail.com";
+$subject = "Mail desde el formulario";
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$comentarios = $_POST['comentario'];
+$message = "
+<html>
+<head>
+<title>HTML</title>
+</head>
+<body>
+<h1>Información del formulario</h1>
+<p>Nombre: $nombre </p> 
+<p>Email: $email </p> 
+<p>Comentarios: $comentarios </p>
+</body>
+</html>";
+    echo 'Gracias por comunicarse, recibirá una respuesta en breve';
+?>
