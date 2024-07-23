624e59a09f5fa1539f5a999ac94bfc927b4f1cbd
Rodrigo <ralonzo1337@gmail.com>
2024-07-23 15:06:44 -0300

    Fixed php

--- php/Form.php
+++ php/Form.php
@@ -1,11 +1,13 @@
 <?php
-$to = "ralonzo1337@gmail.com";
+$to = "info@hotmail.com";
 $subject = "Mail desde el formulario";
 $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+$headers .= "From: " . $_POST['email'] . "\r\n";
+
 $nombre = $_POST['nombre'];
 $email = $_POST['email'];
-$comentarios = $_POST['comentario'];
+$comentarios = $_POST['comentarios'];
 $message = "
 <html>
 <head>
@@ -15,7 +17,7 @@ $message = "
 <h1>Información del formulario</h1>
 <p>Nombre: $nombre </p> 
 <p>Email: $email </p> 
-<p>Comentarios: $comentarios </p>
+<p>Comentarios: $comentario </p>
 </body>
 </html>";
     echo 'Gracias por comunicarse, recibirá una respuesta en breve';
